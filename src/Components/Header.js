85f997a75ea99fe0d94589f078fb8b97356539e8
MatthewIczkowski <iczkom13@gmail.com>
2022-09-30 07:49:41 -0600

    updated header with mui

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,13 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Header = () => {
   const authToken = sessionStorage.getItem('Auth Token');
@@ -12,15 +20,23 @@ const Header = () => {
   const navigate = useNavigate();
 
   return (
-        <nav className="header">
-            <div className="header-text">
-                <h3>Golf Statistics</h3>
-            </div>
-            <div className="nav-buttons">
-              {authToken && <button className="nav-button" onClick={handleLogout}>Log out</button>}
-            </div>
-            
-        </nav>
+    <Box sx={{flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          {/* <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton> */}
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}> Golf Statistics </Typography>
+          {authToken && <Button color="inherit" onClick={handleLogout}>Log out</Button>}
+        </Toolbar>
+      </AppBar>
+    </Box>
   )
 }
 
